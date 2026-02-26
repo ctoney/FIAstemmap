fad2a13bca4b57020b06f572709e1dcbd692e83f
Chris Toney <jctoney@gmail.com>
2026-02-26 16:19:46 -0700

    create_fia_ppp(): initial definition

---
+++ R/create_fia_ppp.R
@@ -0,0 +1,17 @@
+#' Create a 'spatstat' point pattern dataset for an FIA plot tree list
+#' 
+#' `create_fia_ppp()` returns an object of class `"ppp"` representing a point
+#' pattern of an FIA tree list in the 2-D plane. A point pattern object defines
+#' the dataset for working with a stem-mapped FIA plot via functions of the
+#' \pkg{spatstat} package.
+#'
+#' 
+create_fia_ppp <- function(tree_list, live_only = TRUE, min_dia = 5.0,
+                           linear_unit = "ft", macroplot = FALSE,
+                           window = create_fia_owin(linear_unit, macroplot),
+                           mark_cols = c("SPCD", "DIA", "ACTUALHT", "CRWIDTH"),
+                           mark_as_factor = c("SPCD")) {
+
+
+
+}
