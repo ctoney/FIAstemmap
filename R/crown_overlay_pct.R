7b8918858cf99ba0f6690a5287bf864f8b1a2388
Chris Toney <jctoney@gmail.com>
2026-02-25 19:18:26 -0700

    rename arguments

--- R/crown_overlay_pct.R
+++ R/crown_overlay_pct.R
@@ -1,27 +1,30 @@
 #' Compute fractional tree canopy cover of a subplot/microplot by crown overlay
 #'
-#' @param radius A numeric value giving the radius of the subplot/microplot
-#' @param trees A data frame containing the tree list. Must have columns `DIST`
-#' (stem distance from subplot center in same units as `radius`), `AZIMUTH`
-#' (horizontal angle from subplot center to the stem location, `0:359`) and
-#' `CRWIDTH` (tree crown width in the same units as `radius` and `DIST`).
+#' @param plot_radius A numeric value giving the radius of the
+#' subplot/microplot.
+#' @param tree_list A data frame containing tree records for the
+#' subplot/microplot. Must have columns `DIST` (stem distance from subplot
+#' center in same units as `plot_radius`), `AZIMUTH` (horizontal angle from
+#' subplot/microplot center to the stem location, in the range `0:359`) and
+#' `CRWIDTH` (tree crown width in the same units as `plot_radius` and `DIST`).
 #' @return
 #' A numeric value for tree canopy cover as percent of the subplot/microplot
 #' covered by a vertical projection of circular crowns.
 #'
 #' @export
-crown_overlay_pct <- function(radius, trees) {
-    x <- trees$DIST * sin(trees$AZIMUTH * (pi / 180))
-    y <- trees$DIST * cos(trees$AZIMUTH * (pi / 180))
+crown_overlay_pct <- function(plot_radius, tree_list) {
+    x <- tree_list$DIST * sin(tree_list$AZIMUTH * (pi / 180))
+    y <- tree_list$DIST * cos(tree_list$AZIMUTH * (pi / 180))
 
-    crowns <- lapply(seq_len(nrow(trees)), \(i) {
+    crowns <- lapply(seq_len(nrow(tree_list)), \(i) {
         gdalraster::g_create("POINT", c(x[i], y[i])) |>
-            gdalraster::g_buffer(trees$CRWIDTH[i] / 2)
+            gdalraster::g_buffer(tree_list$CRWIDTH[i] / 2)
     })
 
     crowns_poly <- gdalraster::g_build_collection(crowns) |>
         gdalraster::g_unary_union()
-    plot_poly <- gdalraster::g_buffer("POINT (0 0)", radius, quad_segs = 60L)
+
+    plot_poly <- gdalraster::g_buffer("POINT (0 0)", plot_radius, 90L)
 
     gdalraster::g_intersection(plot_poly, crowns_poly) |>
         gdalraster::g_area() / gdalraster::g_area(plot_poly) * 100
