d814f9f2198cafb0b7c134886dadf596835adc02
Chris Toney <jctoney@gmail.com>
2026-02-28 19:44:39 -0700

    rename crwidth_coef to cw_coef and update its documentation

--- R/data.R
+++ R/data.R
@@ -7,23 +7,26 @@
 #' The regression equation is of the general form:
 #'
 #' ```
-#' CRWIDTH = b0 + b1 * DIA + b2 * DIA^2
+#' CW = b0 + b1 * DIA + b2 * DIA^2
 #' ```
 #'
-#' A quadratic term is not included in all cases. `CRWIDTH` is the
-#' predicted tree crown diameter in feet, and `DIA` is FIA stem diameter in
-#' inches.
+#' where `CW` is the predicted tree crown diameter in feet, `DIA` is FIA stem
+#' diameter in inches, and `b0`, `b1`, `b2` are the regression coefficients.
+#' The quadratic term `b2` was not included in the regression models for some
+#' species, and has been assigned `0` in that case for purposes of this lookup
+#' table.
 #'
 #' In cases that species-specific equations were not available in the
 #' literature, surrogate species were assigned based on subjectively similar
 #' tree physiognomy.
 #'
-#' @format ## `crwidth_coef`
+#' @format ## `cw_coef`
 #' A data frame with 430 rows and 8 columns:
 #' \describe{
 #'   \item{\code{symbol}}{\code{\link{character}},
 #'     USDA PLANTS Database species symbol}
-#'   \item{\code{SPCD}}{\code{\link{integer}}, FIA tree species code}
+#'   \item{\code{SPCD}}{\code{\link{integer}},
+#'     FIA tree species code or `-1`}
 #'   \item{\code{common_name}}{\code{\link{character}},
 #'     FIA tree species common name}
 #'   \item{\code{surrogate}}{\code{\link{character}},
@@ -34,6 +37,12 @@
 #'   \item{\code{reference}}{\code{\link{character}},
 #'     Literature source of the species coefficients (see References)}
 #' }
+#' @examples
+#' cw_coef[cw_coef$SPCD == 17, ]
+#' @source
+#' Toney et al. 2009. A stem-map model for predicting tree canopy cover of
+#' Forest Inventory and Analysis (FIA) plots.
+#' \url{https://research.fs.usda.gov/treesearch/33381}.
 #' @references
 #' Bechtold, W.A. 2003. Crown-Diameter Prediction Models for 87 Species of
 #' Stand-Grown Trees in the Eastern United States. _Southern Journal of Applied
@@ -46,7 +55,7 @@
 #' Gill, S.J., G.S. Biging, E.C. Murphy. 2000. Modeling conifer tree crown
 #' radius and estimating canopy cover. _Forest Ecology and Management_, 126,
 #' 405-416.
-"crwidth_coef"
+"cw_coef"
 
 
 #' Tree list for a plot in a loblolly pine plantation
